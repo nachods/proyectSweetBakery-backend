730779b118fbec9f479b023b504a5f2ec8a13f21
nachods <nachodesimone36@gmail.com>
2024-10-04 17:12:43 -0300

    Proyecto terminado con correcciones

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
 require("dotenv").config();
-const express = require("express"); 
+const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const multer = require("multer"); // Importar multer
 const apiVersion = process.env.API_VERSION;
 
-//Crear apps
+// Crear apps
 const app = express();
 
-//Config
+// Configuración de CORS
 app.use(cors());
 
-//Importar Rutas
+// Configuración de multer para manejar archivos
+const upload = multer({ dest: 'uploads/' }); // La carpeta donde se almacenan los archivos
+
+// Importar Rutas
 const registerRoutes = require('./router/register');
-const LoginRoutes = require('./router/login');
-const GetMeRoutes = require('./router/getMe');
-const CatalogoRoutes = require('./router/catalogo');
+const loginRoutes = require('./router/login');
+const getMeRoutes = require('./router/getMe');
+const catalogoRoutes = require('./router/catalogo');
 
-//Proceso de Datos
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// Procesamiento de Datos (Limites de body-parser)
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
-//Static Folder
+// Static Folder para servir archivos estáticos
 app.use(express.static("uploads"));
 
-//Configurar Rutas
+// Configurar Rutas con carga de archivos usando multer
+// Suponiendo que 'register' pueda necesitar cargar archivos
 app.use(`/api/${apiVersion}`, registerRoutes);
-app.use(`/api/${apiVersion}`, LoginRoutes);
-app.use(`/api/${apiVersion}`, GetMeRoutes);
-app.use(`/api/${apiVersion}`, CatalogoRoutes);
+app.use(`/api/${apiVersion}/upload`, upload.single('file'), (req, res) => {
+    // Aquí puedes manejar los datos y el archivo
+    console.log(req.file); // Información del archivo cargado
+    console.log(req.body); // Otros datos del formulario
+    res.send('Archivo cargado exitosamente');
+});
+
+// Configurar Rutas para otros endpoints sin carga de archivos
+app.use(`/api/${apiVersion}`, loginRoutes);
+app.use(`/api/${apiVersion}`, getMeRoutes);
+app.use(`/api/${apiVersion}`, catalogoRoutes);
 
 module.exports = app;
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dbPass = process.env.DB_PASS;
 const dbHost = process.env.DB_HOST;
 const ipServer = process.env.IP_SERVER;
 const apiVersion = process.env.API_VERSION;
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 
 const connectDB = async () => {
   try {
